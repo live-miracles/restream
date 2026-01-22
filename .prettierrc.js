ac892bc7aba4ee51566db762bb404b2a4feafaf6
Nandisha <197202821+nandisha0@users.noreply.github.com>
2026-01-22 07:59:21 +0000

    update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,6 @@ module.exports = {
     singleQuote: true,
     semi: true,
     trailingComma: 'all',
-    arrowParens: 'always',
     bracketSameLine: true,
     plugins: ['prettier-plugin-tailwindcss'],
     overrides: [
